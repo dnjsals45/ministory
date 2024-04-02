15aa38185b0975da736e321793063980072cdd7c
dnjsals45 <dnjsals45@naver.com>
2024-04-02 16:26:32 +0900

    fix | 로그아웃 에러 수정(useEffect 추가)

--- front/app/logout/page.tsx
+++ front/app/logout/page.tsx
@@ -1,17 +1,16 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { AuthContext } from '@/components/hooks/useAuth'
 
 export default function LogOut() {
   const router = useRouter()
   const { logout } = useContext(AuthContext)
-  if (typeof window !== 'undefined') {
-    localStorage.removeItem('access-token')
+
+  useEffect(() => {
     logout()
     router.push('/')
-  }
-
+  }, [logout, router])
   return null
 }
