df2ba3897e6aae1731c03489902201ea210ef134
dnjsals45 <dnjsals45@naver.com>
2024-04-11 17:37:40 +0900

    test | github action workflow test

--- back/ministory/src/main/java/seongmin/ministory/common/jwt/filter/CustomJwtFilter.java
+++ back/ministory/src/main/java/seongmin/ministory/common/jwt/filter/CustomJwtFilter.java
@@ -41,13 +41,13 @@ public class CustomJwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.warn("request: {}@{}", request.getMethod(), request.getRequestURI());
         if (ignoreTokenRequest(request)) {
-            log.info("JWT Filter: Ignoring request: {}", request.getRequestURI());
+//            log.info("JWT Filter: Ignoring request: {}", request.getRequestURI());
             filterChain.doFilter(request, response);
             return;
         }
 
         if (isAnonymousRequest(request)) {
-            log.info("JWT Filter: Anonymous request: {}", request.getRequestURI());
+//            log.info("JWT Filter: Anonymous request: {}", request.getRequestURI());
             filterChain.doFilter(request, response);
             return;
         }
