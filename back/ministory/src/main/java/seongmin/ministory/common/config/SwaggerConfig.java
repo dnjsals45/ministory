f50361f2de0031856c5188b23a972056fb1272f2
dnjsals45 <dnjsals45@naver.com>
2024-04-15 19:21:46 +0900

    modify | swagger https 서버 등록

--- back/ministory/src/main/java/seongmin/ministory/common/config/SwaggerConfig.java
+++ back/ministory/src/main/java/seongmin/ministory/common/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -23,6 +26,9 @@ public class SwaggerConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList("AccessToken"));
+                .addSecurityItem(new SecurityRequirement().addList("AccessToken"))
+                .servers(List.of(new Server().url("https://api.dev-min.me")
+                        .description("실제 운영 서버"),
+                        new Server().url("http://localhost:8080").description("개발 서버")));
     }
 }
