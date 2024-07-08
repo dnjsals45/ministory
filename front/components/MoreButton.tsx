b9bffe0890e45ad5f43b7413cc66fef6a1c26aa3
dnjsals45 <dnjsals45@naver.com>
2024-07-08 17:10:24 +0900

    feat | more 버튼 클릭했을 시 자동으로 닫히도록 변경

--- front/components/MoreButton.tsx
+++ front/components/MoreButton.tsx
@@ -1,22 +1,36 @@
-import { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
-import MorePanel from '@/components/MorePanel'
+import MorePanel from './MorePanel'
 
 export default function MoreButton() {
   const [open, setOpen] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
 
   const toggleOpen = () => {
     setOpen(!open)
   }
 
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref])
+
   return (
-    <div className="relative inline-flex">
+    <div className="relative inline-flex" ref={ref}>
       <button onClick={toggleOpen}>
         <MoreVertIcon />
       </button>
       {open && (
         <div className="absolute right-0 top-full z-10 mt-2 rounded-md bg-white shadow-md dark:bg-gray-800">
-          <MorePanel />
+          <MorePanel setOpen={setOpen} />
         </div>
       )}
     </div>
