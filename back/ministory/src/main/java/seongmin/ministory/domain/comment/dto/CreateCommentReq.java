bbdbcbcd9d4682b42945b8f8d993bc787c554ae7
dnjsals45 <dnjsals45@naver.com>
2024-07-15 13:19:43 +0900

    fix | 댓글 생성 dto @NotBlank 추가

--- back/ministory/src/main/java/seongmin/ministory/domain/comment/dto/CreateCommentReq.java
+++ back/ministory/src/main/java/seongmin/ministory/domain/comment/dto/CreateCommentReq.java
@@ -1,6 +1,7 @@
 package seongmin.ministory.domain.comment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Builder
@@ -9,5 +10,6 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "댓글 생성 요청 DTO")
 public class CreateCommentReq {
+    @NotBlank
     private String comment;
 }
